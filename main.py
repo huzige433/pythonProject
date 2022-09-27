e505a2bde9afa9675bb3b5420e8361ab3175ddd7
hufeng <414272559@qq.com>
2022-09-27 14:10:15 +0800

    合并模块到一个文件

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 # coding=utf-8
 import re
-
+import paramiko
 import requests
 
+
+class LinuxUtil:
+    def __init__(self, ip, port, name, pwd):
+        """
+        连接Linux服务器 并执行命令
+        :param ip:
+        :param port:
+        :param name:
+        :param pwd:
+        """
+        # 创建SSHClient实例对象
+        self.ssh = paramiko.SSHClient()
+        # 调用方法，标识没有远程机器的公钥，允许访问
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 连接远程机器 地址端口用户名密码
+        self.ssh.connect(ip, port, name, pwd)
+
+    def execCmd(self, cmdStr):
+        """
+        执行命令
+        :param cmdStr: 多个命令用分号隔开
+        :return:
+        """
+        # 执行命令（多个命令用分号隔开）
+        stdin, stdout, stderr = self.ssh.exec_command(cmdStr)
+
+        # 获取屏幕上的每行数据
+        return stdout.read().decode().split('\n')
+
+    def close(self):
+        self.ssh.close()
+
+
+
 #第一个检测green的url
-from LinuxUtil import LinuxUtil
-
-
 search_url1="https://www.****.com/srarch1.html"
 #第二个检测green的url
 search_url2="https://www.****.com/srarch2.html"
